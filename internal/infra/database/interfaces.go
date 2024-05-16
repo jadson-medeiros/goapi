2f81c1486be129595dae03afdd949ddd51a15061
Jadson Medeiros <developer.medeiros@gmail.com>
2024-05-16 13:00:54 -0300

    added product db

--- internal/infra/database/interfaces.go
+++ internal/infra/database/interfaces.go
@@ -2,7 +2,15 @@ package database
 
 import "github.com/jadson-medeiros/goapi/internal/entity"
 
-type Userinterface interface {
+type UserInterface interface {
 	Create(user *entity.User) error
 	FindByEmail(email string) (*entity.User, error)
+}
+
+type ProductInterface interface {
+	Create(product *entity.Product) error
+	FindAll(page, limit int, sort string) ([]entity.Product, error)
+	FindByID(id string) (*entity.Product, error)
+	Update(product *entity.Product) error
+	Delete(id string) error
 }
